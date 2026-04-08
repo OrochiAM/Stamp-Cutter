42f6e7d6be0c4fda72b1af376070d1212cf21b64
Aleksa Milic <32849778+OrochiAM@users.noreply.github.com>
2026-04-08 14:20:11 +0200

    Image Cutting Prototype.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 const imageToCut = document.querySelector('.image');
+
 const cutterArea = document.querySelector('.cutter-area');
 
 const isInside = (object, area) => {};
@@ -10,9 +11,29 @@ Draggable.create('.stamp-cutter', {
 
     const cutterRect = cutterArea.getBoundingClientRect();
 
+    let width = cutterRect.width;
+    let height = cutterRect.height;
+
     const cutterX = cutterRect.x;
     const cutterY = cutterRect.y;
 
+    const imageRect = imageToCut.getBoundingClientRect();
+
+    const imageX = imageRect.x;
+    const imageY = imageRect.y;
+
+    imageToCut.style.width = `${width}px`;
+    imageToCut.style.height = `${height}px`;
+
+    imageToCut.style.width = `${width}px`;
+    imageToCut.style.height = `${height}px`;
+
+    imageToCut.style.left = `${cutterX}px`;
+    imageToCut.style.top = `${cutterY}px`;
+
+    imageToCut.style.backgroundPosition = `-${cutterX}px -${cutterY}px`;
+
+    console.log(cutterX);
     console.log(`X:${cutterX} \n Y:${cutterY}`);
   },
   onDragEnd: function () {
