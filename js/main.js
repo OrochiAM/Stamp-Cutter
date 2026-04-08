daa71ef5c27bd7e7d144db9d282aac8045f4f324
Aleksa Milic <32849778+OrochiAM@users.noreply.github.com>
2026-04-08 13:04:13 +0200

    Cutter Position.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,19 @@
+const imageToCut = document.querySelector('.image');
+const cutterArea = document.querySelector('.cutter-area');
+
+const isInside = (object, area) => {};
+
 Draggable.create('.stamp-cutter', {
-  bounds: document.body,
   inertia: true,
   onClick: function () {
     console.log('clicked');
+
+    const cutterRect = cutterArea.getBoundingClientRect();
+
+    const cutterX = cutterRect.x;
+    const cutterY = cutterRect.y;
+
+    console.log(`X:${cutterX} \n Y:${cutterY}`);
   },
   onDragEnd: function () {
     console.log('drag ended');
